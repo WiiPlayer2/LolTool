3f554f68f2f12897fa287ca0af6937307897ba64
WiiPlayer2 <waldetomme@gmail.com>
2016-08-25 17:37:33 +0200

    Initial Commit

---
+++ LoLTool/MainApp.cs
@@ -0,0 +1,136 @@
+﻿using CLAP;
+using CLAP.Validation;
+using LoLTool.Properties;
+using RiotSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace LoLTool
+{
+    class MainApp
+    {
+        private static RiotApi riotApi;
+
+        public static RiotApi Api
+        {
+            get
+            {
+                if (riotApi != null)
+                {
+                    return riotApi;
+                }
+                if (!string.IsNullOrWhiteSpace(Settings.Default.ApiKey))
+                {
+                    riotApi = RiotApi.GetInstance(Settings.Default.ApiKey);
+                }
+                return riotApi;
+            }
+        }
+
+        [Empty, Help, Verb(IsDefault = true)]
+        public static void Help(string help)
+        {
+            Console.WriteLine("LolTool 0.1");
+            Console.WriteLine("Copyright © DarkLink 2016");
+            Console.WriteLine(help);
+        }
+
+        [Global]
+        public static void SetApiKey(string key)
+        {
+            Settings.Default.ApiKey = key;
+            Settings.Default.Save();
+        }
+
+        [Global]
+        public static void SetRegion(Region region)
+        {
+            Settings.Default.Region = region;
+            Settings.Default.Save();
+        }
+
+        private static Platform Platform
+        {
+            get
+            {
+                switch (Settings.Default.Region)
+                {
+                    case Region.euw:
+                        return Platform.EUW1;
+                    default:
+                        throw new NotImplementedException(Settings.Default.Region.ToString());
+                }
+            }
+        }
+
+        [Verb]
+        public static void Chime(string username,
+            [DefaultValue(5), MoreThan(0)]int checkRate,
+            string sound)
+        {
+            var summoner = Api.GetSummoner(Settings.Default.Region, username);
+            RiotSharp.CurrentGameEndpoint.CurrentGame game = null;
+            try
+            {
+                game = Api.GetCurrentGame(Platform, summoner.Id);
+                Console.WriteLine("Mode:      {0}", game.GameMode);
+                Console.WriteLine("QueueType: {0}", game.GameQueueType);
+                Console.WriteLine("GameType:  {0}", game.GameType);
+                Console.WriteLine("StartTime: {0}", game.GameStartTime.ToLocalTime());
+                Console.WriteLine();
+            }
+            catch (RiotSharpException)
+            {
+                Console.WriteLine("No game found.");
+                return;
+            }
+
+            var top = Console.CursorTop;
+            while (game != null)
+            {
+                try
+                {
+                    game = Api.GetCurrentGame(Platform, summoner.Id);
+
+                    Console.CursorTop = top;
+                    Console.WriteLine("Game is running for {0}", TimeSpan.FromSeconds(game.GameLength));
+                }
+                catch (RiotSharpException e)
+                {
+                    if (e.Message.StartsWith("404,"))
+                    {
+                        game = null;
+                    }
+                }
+                Thread.Sleep(1000 * checkRate);
+            }
+
+            Console.WriteLine("Game finished.");
+
+            SoundPlayer sndPlayer;
+            if (sound == null)
+            {
+                sndPlayer = new SoundPlayer(
+                    Assembly.GetExecutingAssembly().GetManifestResourceStream("LoLTool.ChimeSound.wav"));
+            }
+            else
+            {
+                sndPlayer = new SoundPlayer(Path.GetFullPath(sound));
+            }
+            sndPlayer.PlaySync();
+        }
+
+        [Error]
+        public static void HandleError(ExceptionContext context)
+        {
+            Console.WriteLine(context.Exception);
+        }
+    }
+}
---
+++ LoLTool/Program.cs
@@ -0,0 +1,106 @@
+﻿using RiotSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoLTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if DEBUG && ONE_TIME
+            args = new string[] { "addjob", "/name=test" };
+            var prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine(":{0}", string.Join(" ", args.Select(o =>
+            {
+                if (o.Contains(" "))
+                {
+                    return string.Format("\"{0}\"", o);
+                }
+                return o;
+            })));
+            Console.WriteLine();
+            Console.ForegroundColor = prevColor;
+#elif DEBUG
+            while (true)
+            {
+                var argSuccess = false;
+                var prevColor = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Cyan;
+                do
+                {
+                    try
+                    {
+                        Console.Write(":");
+                        var line = Console.ReadLine();
+                        var argsList = new List<string>();
+                        var str = new StringBuilder();
+                        var inQuote = false;
+                        foreach (var c in line)
+                        {
+                            switch (c)
+                            {
+                                case ' ':
+                                    if (!inQuote)
+                                    {
+                                        argsList.Add(str.ToString());
+                                        str.Clear();
+                                    }
+                                    else
+                                    {
+                                        str.Append(c);
+                                    }
+                                    break;
+                                case '\"':
+                                    inQuote = !inQuote;
+                                    break;
+                                default:
+                                    str.Append(c);
+                                    break;
+                            }
+                        }
+                        argsList.Add(str.ToString());
+
+                        args = argsList
+                            .Where(o => !string.IsNullOrEmpty(o))
+                            .ToArray();
+                        argSuccess = true;
+                    }
+                    catch
+                    {
+                        argSuccess = false;
+                    }
+                }
+                while (!argSuccess);
+                Console.ForegroundColor = prevColor;
+
+                try
+                {
+#endif
+
+                    CLAP.Parser.Run<MainApp>(args);
+
+#if DEBUG && ONE_TIME
+            prevColor = Console.ForegroundColor;
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("[Press any key to exit...]");
+            Console.ReadKey(true);
+            Console.ForegroundColor = prevColor;
+#elif DEBUG
+                }
+                catch (Exception e)
+                {
+                    prevColor = Console.ForegroundColor;
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(e);
+                    Console.ForegroundColor = prevColor;
+                }
+            }
+#endif
+        }
+    }
+}
